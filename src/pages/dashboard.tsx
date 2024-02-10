5fb06fe3274a98b67e08a05519a17bc55f96eb59
kambhani <63814718+kambhani@users.noreply.github.com>
2024-02-10 13:58:54 -0500

    Update dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,47 @@
 import Link from "next/link";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "~/components/ui/card";
 
 export default function Dashboard() {
   return (
-    <div className="mx-auto w-full px-2 md:w-11/12">
-      <Link href="/upload-resume">Upload resume</Link>
-      <Link href="/profile">Edit profile information</Link>
-      <Link href="/generate">Generate new resume</Link>
+    <div className="mx-auto grid w-full grid-cols-1 gap-8 px-2 md:w-11/12 lg:grid-cols-3">
+      <Link href="/upload-resume">
+        <Card className="h-full duration-300 hover:scale-[1.03]">
+          <CardHeader>
+            <CardTitle>Upload Resume</CardTitle>
+            <CardDescription>
+              Upload your resume for processing on our server. This will let us
+              parse the information and load it to your profile.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </Link>
+      <Link href="/profile">
+        <Card className="h-full duration-300 hover:scale-[1.03]">
+          <CardHeader>
+            <CardTitle>Edit Profile Information</CardTitle>
+            <CardDescription>
+              Add education, experience, and more to your profile. This will
+              help our AI generate the best resume for you.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </Link>
+      <Link href="/generate">
+        <Card className="h-full duration-300 hover:scale-[1.03]">
+          <CardHeader>
+            <CardTitle>Generate Resume</CardTitle>
+            <CardDescription>
+              Generate a custom resume and cover letter based on your profile
+              and job description.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </Link>
     </div>
   );
 }
