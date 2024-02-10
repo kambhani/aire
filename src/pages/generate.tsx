0930358ff790adaabbbfad267d9eddbe38ac0990
Ganning Xu <ganningxu@gmail.com>
2024-02-10 13:47:06 -0500

    generate page and navbar and footeRg

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,4 +1,5 @@
 import { Textarea } from "~/components/ui/textarea";
+import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
@@ -6,10 +7,52 @@ import { useState } from "react";
 export default function Generate() {
   const { data: sessionData } = useSession();
   const [description, setDescription] = useState("");
+  const [url, setUrl] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   return (
-    <div className="mx-auto mt-12 grid w-full grid-cols-1 gap-8 px-8 md:grid-cols-2 lg:w-11/12">
-      <div className="">
+    <div className="mx-auto mt-24 grid w-full px-8">
+
+      {
+        !submitted && (
+          <div className='mx-auto'>
+            <h1 className='text-xl font-bold'>Job Description</h1>
+            <p className=' text-slate-400 text-sm'>Please copy and paste the job description for the posting you have found below</p>
+            <Textarea
+              className="mb-4 h-80 max-w-xl mx-auto mt-4"
+              placeholder="enter the job description here..."
+              value={description}
+              onChange={(newDescription) =>
+                setDescription(newDescription.target.value)
+              }
+            />
+
+
+            <p className='text-lg font-bold'>Or, enter the job posting URL:</p>
+
+            <Input
+              className="mb-4 max-w-xl mx-auto mt-4"
+              placeholder="https://www.example.com"
+              value={description}
+              onChange={(newURL) =>
+                setUrl(newURL.target.value)
+              }
+            />
+
+
+            <Button className='bg-black'>Submit</Button>
+
+
+
+
+
+
+          </div>
+
+
+        )
+      }
+      {/* <div className="">
         <h2 className="mb-4 text-4xl">Paste job description here</h2>
         <Textarea
           className="mb-4 h-80 w-full"
@@ -25,7 +68,7 @@ export default function Generate() {
         <div className="h-80 w-full border border-slate-900 dark:border-slate-100">
           outputted resume goes here
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
